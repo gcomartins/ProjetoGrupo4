69b1293a2b77d0a73ee117f53c864634e72b75d6
Guilherme Carneiro <gcomartins@gmail.com>
2022-04-10 13:07:04 -0300

    Alterações nos formulários pra acoplar com BD

--- Site/api-site/src/database/config.js
+++ Site/api-site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     user: "root",
     database: "HomoUniversalis",
-    password: "Chinelinho@12",
+    password: "XXX",
 };
 
 function executar(instrucao) {
