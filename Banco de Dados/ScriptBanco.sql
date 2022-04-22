bc7f8b91efdd40a57fef94ca50fa829f0bb384ae
ViniciusPieroni <vinizanetini@gmail.com>
2022-04-22 04:18:45 -0300

    Cadastro maquina e cadastro funcionario feitos

--- Banco de Dados/ScriptBanco.sql
+++ Banco de Dados/ScriptBanco.sql
@@ -25,18 +25,20 @@ foreign key (fkEmpresa) references tbEmpresas (idEmpresa));
 select M.*, C.nome, C.capacidade, C.limiteAlerta, C.limiteTemperatura from tbMaquinas as M 
 inner join tbComponentes as C on M.idmaquina =  C.fkMaquina where hostName = "Lucas";
 
+
+
 create table tbUsuarios (
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nomeUsuario VARCHAR(45),
+sobrenomeUsuario VARCHAR(45),
 email VARCHAR(45) unique,
 senha VARCHAR(45),
+cargo VARCHAR(45),
 fkEmpresa INT,
 constraint EmpresaUsuario 
-foreign key (fkEmpresa) references tbEmpresas (idEmpresa),
-sudo TINYINT,
-administrador TINYINT,
-usuario TINYINT
+foreign key (fkEmpresa) references tbEmpresas (idEmpresa)
 );
+
 
 
 
@@ -145,4 +147,5 @@ insert into tbAlertas (fkLog, categoria, descrição) values
 select * from tbLogs;
 select * from tbMaquinas;
 select * from tbEmpresas;
+select * from tbUsuarios;
 
