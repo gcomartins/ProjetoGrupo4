e909e1038e01e38d681dec6f9063f7381e84d488
ViniciusPieroni <vinizanetini@gmail.com>
2022-04-27 14:44:40 -0300

    Melhorias no formulario e organizando BE

---
+++ Site/api-site/public/js/Listar.js
@@ -0,0 +1,106 @@
+function listar(){
+    fetch(`/usuarios/listar`, {
+        method: "GET",
+    }).then(function (resposta) {
+    
+        // console.log("resposta: ", resposta);
+        resposta.json().then((json) => {
+            var obj = (json);
+            console.log(obj)
+            for(var i = 0; i < 20; i++) {
+                        lista.innerHTML += `
+                <div class="box-informacoes-cadastradas">
+                
+                <div class="titulos">
+                <h4 class="title-ram">${obj[i].idUsuario}</h4>
+                <h4 class="title-ram">${obj[i].nomeUsuario} ${obj[i].sobrenomeUsuario}</h4>
+                <h4 class="title-alerta">${obj[i].email}</h4>
+                <h4 class="title-alerta">${obj[i].cargo}</h4>
+                <div class="wrapper">
+                    <div class="icon editar">
+                        <div  class="tooltip">Editar</div>
+                        <span id="editar-div" class="icons">
+                        <i id="editar" class="material-symbols-sharp">border_color</i>
+                    </span>
+                    </div>
+                </div>
+                <div class="wrapper">
+                    <div class="icon excluir">
+                        <div  id="tooltip-excluir" class="tooltip">Excluir</div>
+                        <span id="deletar-div" class="icons">
+                        <i id="deletar" class="material-symbols-sharp">delete</i>
+                        </span>
+                    </div>
+                </div>
+              </div>
+            </div>
+    
+                        `
+                    }
+        }).catch((erro) => {
+    
+        })
+    
+    }).catch(function (resposta) {
+        if (resposta.status == 406) {
+            alert('Oops!! Parece que você ja curtiu esse livro... que tal procurar um outro que tenha interesse? :)')
+        }
+        console.log(`#ERRO:${resposta}`);
+    
+    });
+    }
+
+
+function listarMaquina(){
+        fetch(`/usuarios/listarMaquina`, {
+            method: "GET",
+        }).then(function (resposta) {
+        
+            // console.log("resposta: ", resposta);
+            resposta.json().then((json) => {
+                var obj = (json);
+                console.log(obj)
+                for(var i = 0; i < 20; i++) {
+                        listaMaquina.innerHTML += `
+                    <div class="box-informacoes-cadastradas">
+                    
+                    <div class="titulos">
+                    <h4 class="title-ram">${obj[i].idMaquinaComponentes}</h4>
+                    <h4 class="title-alerta">${obj[i].nome}</h4>
+                    <h4 class="title-alerta">${obj[i].capacidade}</h4>
+                    <h4 class="title-alerta">${obj[i].limiteAlerta}%</h4>
+                    <div class="wrapper">
+                        <div class="icon editar">
+                            <div  class="tooltip">Editar</div>
+                            <span id="editar-div" class="icons">
+                            <i id="editar" class="material-symbols-sharp">border_color</i>
+                        </span>
+                        </div>
+                    </div>
+                    <div class="wrapper">
+                        <div class="icon excluir">
+                            <div  id="tooltip-excluir" class="tooltip">Excluir</div>
+                            <span id="deletar-div" class="icons">
+                            <i id="deletar" class="material-symbols-sharp">delete</i>
+                            </span>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+        
+                            `
+                        }
+            }).catch((erro) => {
+        
+            })
+        
+        }).catch(function (resposta) {
+            if (resposta.status == 406) {
+                alert('Oops!! Parece que você ja curtiu esse livro... que tal procurar um outro que tenha interesse? :)')
+            }
+            console.log(`#ERRO:${resposta}`);
+        
+        });
+    }
+        
+        
