39cdce66b94480d3d53f7d52c299915eb4876456
ViniciusPieroni <vinizanetini@gmail.com>
2022-05-23 10:16:32 -0300

    utilizando medidas e arrumado listar maquinas

--- Site/api-site/src/models/usuarioModel.js
+++ Site/api-site/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function listarMaquina() {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarMaquina()");
     var instrucao = `
-         SELECT * FROM tbMaquinasComponentes;
+         SELECT * FROM tbMaquinas;
          
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
